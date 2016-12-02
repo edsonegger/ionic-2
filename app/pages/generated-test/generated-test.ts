d8f71b4d27491017a011f5ff41e6c19334d851f1
Edson Edgar Egger <edson.egger@gmail.com>
2016-12-02 09:19:43 -0200

    teste chamda rest para api via cep

--- app/pages/generated-test/generated-test.ts
+++ app/pages/generated-test/generated-test.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ConnectionService} from './../../providers/connection-service/connection-service';
 
 /*
   Generated class for the GeneratedTestPage page.
@@ -11,5 +12,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/generated-test/generated-test.html',
 })
 export class GeneratedTestPage {
-  constructor(private nav: NavController) {}
+  constructor(private nav: NavController, private connectionService: ConnectionService) {}
+
+buscarCep() : void {
+	this.connectionService.getCep('80510325')
+	.then((res) => {
+		let json = res.json();
+		console.log(json);			
+		
+	}).catch((err) => {
+		console.log(err);
+	});
 }
+}
+
+
